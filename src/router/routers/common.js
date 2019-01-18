b7d572395cded331e07299e602766064af96e43f
lihaiwo <249982536@163.com>
2019-01-18 10:50:46 +0800

    管理界面版本0.1.2

--- src/router/routers/common.js
+++ src/router/routers/common.js
@@ -1,12 +1,4 @@
 export default [{
-  path: '/login',
-  name: 'login',
-  component: () => import ('@/view/login/login')
-},{
-  path: '/',
-  name: 'welcome',
-  component: () => import('@/view/welcome.vue')
-},{
   path: '/401',
   name: 'error_401',
   component: () => import('@/view/error-page/401.vue')
