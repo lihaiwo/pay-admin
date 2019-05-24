6c6ce9edc0d6f411601340eb93822780697d6a99
lihaiwo <249982536@163.com>
2019-05-24 13:35:30 +0800

    admin保存

--- src/router/routers/admin.js
+++ src/router/routers/admin.js
@@ -2,7 +2,7 @@ export default [
   {path: '/admin',component: () => import('@/view/layout/index/admin'),
     children: [
       // 基本组件
-      {path: '',component: () => import('@/view/admin/home')},
+      {path: '',component: () => import('@/view/layout/home')},
       {path: 'system/duty',component: () => import('@/view/system/duty/index.vue')},
       {path: 'system/import',component: () => import('@/view/system/import/index.vue')},
       {path: 'system/svg',component: () => import('@/view/system/svg/index.vue')},
